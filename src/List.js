701767552d54b6fe0caa969eb73ee0aaac3e04d9
Quentin Lurkin <qlurkin@gmail.com>
2018-03-19 21:14:56 +0100

    Add keys to Student List

--- src/List.js
+++ src/List.js
@@ -6,7 +6,7 @@ class List extends Component {
   render() {
     return (
         <div>
-            {Object.keys(this.props.students).map(matricule => <Student name={this.props.students[matricule].name} matricule={matricule} division={this.props.students[matricule].division}/>)}
+            {Object.keys(this.props.students).map(matricule => <Student name={this.props.students[matricule].name} matricule={matricule} division={this.props.students[matricule].division} key={matricule}/>)}
         </div>   
     );
   }
